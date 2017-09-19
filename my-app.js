0b1dc821ab3f1ad59e1ef9254c08c4dae3658ab4
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-09-19 10:15:28 +0530

    changes made to new faeture,main branch untouched

--- my-app.js
+++ my-app.js
@@ -1,6 +1,11 @@
+function anything(){
+console.log('a lot of work');
+console.log('half way through');
+}
 function something()
 {
  console.log('i am something');
 }
 console.log('hello world');
 something();
+anything();
